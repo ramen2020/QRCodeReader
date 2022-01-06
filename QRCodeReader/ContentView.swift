18154a45e03f03a28d3cb32021edd4ee441a676e
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-06 17:17:16 +0900

    init

---
+++ QRCodeReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeReader
+//
+//  Created by 宮本光直 on 2022/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeReader/QRCodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeReaderApp.swift
+//  QRCodeReader
+//
+//  Created by 宮本光直 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
