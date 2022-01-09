fd614f4694f9060db4b6701b400dda5f4f22401a
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-10 07:23:01 +0900

    add ScanResultType enum and ScanAlert struct to show Alert

---
+++ QRCodeReader/ScanAlert.swift
@@ -0,0 +1,13 @@
+//
+//  ScanAlert.swift
+//  QRCodeReader
+//
+//  Created by 宮本光直 on 2022/01/10.
+//
+
+import SwiftUI
+
+struct ScanAlert: Identifiable {
+    var id = UUID()
+    var result: ScanResultType
+}
---
+++ QRCodeReader/ScanResultType.swift
@@ -0,0 +1,13 @@
+//
+//  ScanResultType.swift
+//  QRCodeReader
+//
+//  Created by 宮本光直 on 2022/01/10.
+//
+
+import SwiftUI
+
+enum ScanResultType {
+    case success(message: String)
+    case failure
+}
