d5820a3fd539b8b17299ea38d79cc2d24e814529
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-10 07:49:02 +0900

    add view to use QR

--- QRCodeReader/ContentView.swift
+++ QRCodeReader/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var canScan = true
+    @State var isShowAlert: ScanAlert? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            QRReader(
+                canScan: $canScan,
+                completion: { result in
+                    switch result {
+                    case .success(let code):
+                        isShowAlert = ScanAlert(result: .success(message: code))
+                        print(":::: code: \(code.debugDescription)")
+                    case .failure(let error):
+                        isShowAlert = ScanAlert(result: .failure)
+                        print(":::: error: \(error)")
+                    }
+                }
+            )
+            .ignoresSafeArea(.all)
+        }
+        .alert(item: $isShowAlert) {
+            switch $0.result {
+            case .success(let message):
+                return Alert(title: Text("scan success!!!"),
+                             message: Text("\(message)"),
+                             dismissButton: .default(Text("close"), action: {
+                                canScan = true
+                             }))
+            case .failure:
+                return Alert(title: Text("scan failure!!!"),
+                             dismissButton: .default(Text("close"), action: {
+                                canScan = true
+                             }))
+            }
+        }
     }
 }
 
